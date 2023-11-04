981674173fab34d1d63ebe5a71007ab65751c7dc
NIMCOO <nimco77001@gmail.com>
2023-11-04 07:32:21 +0000

    Well Done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello");
+
+const grade = 92;
+
+if(grade=100/90){
+   console. log("you got an A grade")
+
+}else if(grade=90/75){
+   console. log("you got an B grade")
+
+}else if(grade=75/60){
+   console. log("you got an c grade")
+
+}else if(grade=59/0){
+   console. log("you got an failed")
+}
